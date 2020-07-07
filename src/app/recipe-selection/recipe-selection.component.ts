7cf413dcb66571b3e514b53c138437e14e998b48
Nicolaj Bunting <steelmagnum@hotmail.com>
2020-07-06 22:47:28 -0700

    Updated component to include other groups

--- src/app/recipe-selection/recipe-selection.component.ts
+++ src/app/recipe-selection/recipe-selection.component.ts
@@ -22,16 +22,21 @@ export class RecipeSelectionComponent implements OnInit
 {
   allTypes: string[];
   allStyles: string[];
-  allFamilies: any;
+  allFamilies: string[];
   allPrimaryComponents: string[];
   allSecondaryComponents: string[];
 
   allInventories: Inventory[];
 
   // selectionForm: FormGroup;
-  typesForm: FormGroup;
 
+  typeOptions: Option = {
+    name: 'Type',
+    selected: true,
+    subOptions: []
+  };
   allSelectedTypes: string[] = [];
+  areAllTypesSelected: boolean = true;
 
   styleOptions: Option = {
     name: 'Style',
@@ -39,13 +44,31 @@ export class RecipeSelectionComponent implements OnInit
     subOptions: [ ]
   };
   allSelectedStyles: string[] = [];
-  allStylesSelected: boolean = true;
+  areAllStylesSelected: boolean = true;
 
+  familyOptions: Option = {
+    name: 'Family',
+    selected: true,
+    subOptions: []
+  };
   allSelectedFamilies: string[] = [];
+  areAllFamiliesSelected: boolean = true;
 
+  primaryOptions: Option = {
+    name: 'Main',
+    selected: true,
+    subOptions: []
+  };
   allSelectedPrimaryComponents: string[] = [];
+  areAllPrimaryComponentsSelected: boolean = true;
 
+  secondaryOptions: Option = {
+    name: 'Auxiliary',
+    selected: true,
+    subOptions: []
+  };
   allSelectedSecondaryComponents: string[] = [];
+  areAllSecondaryComponentsSelected: boolean = true;
 
   limitToAvailable: boolean = false;
 
@@ -54,14 +77,6 @@ export class RecipeSelectionComponent implements OnInit
     private recipeComponentService: RecipeComponentsService, 
     private ingredientInventory: IngredientInventoriesService
 ) { }
-
-  get typesArray(): FormArray {
-    return this.typesForm.controls.typesArray as FormArray;
-  }
-
-  // get types() {
-  //   return this.typesForm.get('types');
-  // }
 
   ngOnInit(): void {
     this.recipeCategoryService.GetTypes().subscribe((data: any) => {this.allTypes = data.types;});
@@ -74,17 +89,26 @@ export class RecipeSelectionComponent implements OnInit
 
     //this.selectionForm = this.formBuilder.group({});
 
-    this.typesForm = this.formBuilder.group({ typesArray: this.formBuilder.array([]) });
-    for(let type in this.allTypes) {
-      this.typesArray.push(this.formBuilder.control(true));
+    let allGroups: string[][] = [this.allTypes, this.allStyles, this.allFamilies, 
+      this.allPrimaryComponents, this.allSecondaryComponents];
+    let allOptions: Option[] = [this.typeOptions, this.styleOptions, this.familyOptions,
+      this.primaryOptions, this.secondaryOptions];
+
+    for(let i in allGroups) {
+      let option: Option = allOptions[i];
+
+      let group = allGroups[i];
+      for(let j in group) {
+        let value = group[j];
+        option.subOptions.push({ name: value, selected: true});
+      }
     }
 
-    for(let i in this.allStyles) {
-      let style = this.allStyles[i];
-      this.styleOptions.subOptions.push({ name: style, selected: true});
-    }
-
+    this.updateSelectedTypes();
     this.updateSelectedStyles();
+    this.updateSelectedFamilies();
+    this.updateSelectedPrimaryComponents();
+    this.updateSelectedSecondaryComponents();
   }
 
   // addTypes() {
@@ -115,14 +139,42 @@ export class RecipeSelectionComponent implements OnInit
     console.log(group + ' group checkboxes have been set to ' + selected);  //debug
 
     switch(group) {
+      case "Type":
+        this.setAllTypesSelected(selected);
+        break;
+
       case "Style":
         this.setAllStylesSelected(selected);
         break;
+
+        case "Family":
+          this.setAllFamiliesSelected(selected);
+          break;
+
+          case "Main":
+            this.setAllPrimaryComponentsSelected(selected);
+            break;
+
+            case "Auxiliary":
+              this.setAllSecondaryComponentsSelected(selected);
+              break;
     }
   }
 
+  setAllTypesSelected(selected: boolean) {
+    this.areAllTypesSelected = selected;
+
+    if (this.typeOptions.subOptions == null) {
+      return;
+    }
+
+    this.typeOptions.subOptions.forEach(o => o.selected = selected);
+
+    this.updateSelectedTypes();
+  }
+
   setAllStylesSelected(selected: boolean) {
-    this.allStylesSelected = selected;
+    this.areAllStylesSelected = selected;
 
     if (this.styleOptions.subOptions == null) {
       return;
@@ -133,20 +185,93 @@ export class RecipeSelectionComponent implements OnInit
     this.updateSelectedStyles();
   }
 
+  setAllFamiliesSelected(selected: boolean) {
+    this.areAllFamiliesSelected = selected;
+
+    if (this.familyOptions.subOptions == null) {
+      return;
+    }
+
+    this.familyOptions.subOptions.forEach(o => o.selected = selected);
+
+    this.updateSelectedFamilies();
+  }
+
+  setAllPrimaryComponentsSelected(selected: boolean) {
+    this.areAllPrimaryComponentsSelected = selected;
+
+    if (this.primaryOptions.subOptions == null) {
+      return;
+    }
+
+    this.primaryOptions.subOptions.forEach(o => o.selected = selected);
+
+    this.updateSelectedPrimaryComponents();
+  }
+
+  setAllSecondaryComponentsSelected(selected: boolean) {
+    this.areAllSecondaryComponentsSelected = selected;
+
+    if (this.secondaryOptions.subOptions == null) {
+      return;
+    }
+
+    this.secondaryOptions.subOptions.forEach(o => o.selected = selected);
+
+    this.updateSelectedSecondaryComponents();
+  }
+
   toggleSelected(group: string, name: string, selected: boolean) {
     console.log(group + " " + name + ' checkbox has been set to ' + selected);  //debug
 
     switch(group) {
+      case "Type":
+        this.updateSelectedTypes();
+        break;
+
       case "Style":
         this.updateSelectedStyles();
         break;
+
+        case "Family":
+          this.updateSelectedFamilies();
+          break;
+
+          case "Main":
+            this.updateSelectedPrimaryComponents();
+            break;
+
+            case "Auxiliary":
+              this.updateSelectedSecondaryComponents();
+              break;
+    }
+  }
+
+  updateSelectedTypes() {
+    this.areAllTypesSelected = this.typeOptions.subOptions != null 
+    && this.typeOptions.subOptions.every(o => o.selected);
+
+    if(this.areAllTypesSelected == true) {
+      this.allSelectedTypes = ["all"];
+      return;
+    }
+
+    this.allSelectedTypes = [];
+    for(let i in this.typeOptions.subOptions) {
+      let type = this.typeOptions.subOptions[i];
+      if(type.selected != true) {
+        continue;
+      }
+
+      this.allSelectedTypes.push(type.name);
     }
   }
 
   updateSelectedStyles() {
-    this.allStylesSelected = this.styleOptions.subOptions != null && this.styleOptions.subOptions.every(o => o.selected);
+    this.areAllStylesSelected = this.styleOptions.subOptions != null 
+    && this.styleOptions.subOptions.every(o => o.selected);
 
-    if(this.allStylesSelected == true) {
+    if(this.areAllStylesSelected == true) {
       this.allSelectedStyles = ["all"];
       return;
     }
@@ -159,6 +284,66 @@ export class RecipeSelectionComponent implements OnInit
       }
 
       this.allSelectedStyles.push(style.name);
+    }
+  }
+
+  updateSelectedFamilies() {
+    this.areAllFamiliesSelected = this.familyOptions.subOptions != null 
+    && this.familyOptions.subOptions.every(o => o.selected);
+
+    if(this.areAllFamiliesSelected == true) {
+      this.allSelectedFamilies = ["all"];
+      return;
+    }
+
+    this.allSelectedFamilies = [];
+    for(let i in this.familyOptions.subOptions) {
+      let family = this.familyOptions.subOptions[i];
+      if(family.selected != true) {
+        continue;
+      }
+
+      this.allSelectedFamilies.push(family.name);
+    }
+  }
+
+  updateSelectedPrimaryComponents() {
+    this.areAllPrimaryComponentsSelected = this.primaryOptions.subOptions != null 
+    && this.primaryOptions.subOptions.every(o => o.selected);
+
+    if(this.areAllPrimaryComponentsSelected == true) {
+      this.allSelectedPrimaryComponents = ["all"];
+      return;
+    }
+
+    this.allSelectedPrimaryComponents = [];
+    for(let i in this.primaryOptions.subOptions) {
+      let primaryComponent = this.primaryOptions.subOptions[i];
+      if(primaryComponent.selected != true) {
+        continue;
+      }
+
+      this.allSelectedPrimaryComponents.push(primaryComponent.name);
+    }
+  }
+
+  updateSelectedSecondaryComponents() {
+    this.areAllSecondaryComponentsSelected = this.secondaryOptions.subOptions != null 
+    && this.secondaryOptions.subOptions.every(o => o.selected);
+
+    if(this.areAllSecondaryComponentsSelected == true) {
+      this.allSelectedSecondaryComponents = ["all"];
+      return;
+    }
+
+    this.allSelectedSecondaryComponents = [];
+    for(let i in this.secondaryOptions.subOptions) {
+      let secondaryComponent = this.secondaryOptions.subOptions[i];
+      if(secondaryComponent.selected != true) {
+        continue;
+      }
+
+      this.allSelectedSecondaryComponents.push(secondaryComponent.name);
     }
   }
 }
