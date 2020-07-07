6681ef52905386092aa162e87006614848fc0f5f
Nicolaj Bunting <steelmagnum@hotmail.com>
2020-07-06 20:22:45 -0700

    Change param names. Added recipe IDs to returned values

--- src/app/Services/recipe-list.service.ts
+++ src/app/Services/recipe-list.service.ts
@@ -7,17 +7,17 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class RecipeListService extends AirtableService {
-  requestUrl: string = 'Recipe%20Names?fields[]=Name&fields[]=Variant';
+  requestUrl: string = 'Recipe%20Names?fields[]=Recipe ID&fields[]=Name&fields[]=Variant';
 
     constructor(http: HttpClient) { super(http) }
 
-    GetRecipes(types: string[], styles: string[], families: string[], 
-      primaryComponents: string[], secondaryComponents: string[], limitToAvailable: boolean
+    GetRecipes(allTypes: string[], allStyles: string[], allFamilies: string[], 
+      allPrimaryComponents: string[], allSecondaryComponents: string[], limitToAvailable: boolean
       ): Observable<any> {
       const allRecipes = {"records":[
-        {id: 'recwlMsjYThiXto6x', fields: {name: '20th Century Cocktail'}, createdTime: '2020-04-16T16:37:28.000Z'}, 
-        {id: 'recZpZn7bWbxauSmP', fields: {name: 'Ahumado Seco'}, createdTime: '2020-04-16T18:17:00.000Z'}, 
-        {id: 'recK7mTjXbgoIPryN', fields: {name: 'Appletini', variant: 'Sour apple schnapps'}, createdTime: '2020-04-17T02:53:04.000Z'}
+        {id: 'recwlMsjYThiXto6x', fields: {id: 1, name: '20th Century Cocktail'}, createdTime: '2020-04-16T16:37:28.000Z'}, 
+        {id: 'recZpZn7bWbxauSmP', fields: {id: 2, name: 'Ahumado Seco'}, createdTime: '2020-04-16T18:17:00.000Z'}, 
+        {id: 'recK7mTjXbgoIPryN', fields: {id: 6, name: 'Appletini', variant: 'Sour apple schnapps'}, createdTime: '2020-04-17T02:53:04.000Z'}
       ]};
 
       return of(allRecipes);
