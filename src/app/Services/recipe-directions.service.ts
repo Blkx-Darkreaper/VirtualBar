32c68077bfeddc49e8384aadcb53caa471cd6268
Nicolaj Bunting <steelmagnum@hotmail.com>
2020-07-06 21:15:30 -0700

    Updated services to match airtable responses

--- src/app/Services/recipe-directions.service.ts
+++ src/app/Services/recipe-directions.service.ts
@@ -32,18 +32,112 @@ export class RecipeDirectionsService extends AirtableService {
   }*/
 
   GetDirections(recipeId: number): Observable<any> {
-    let allDirections = {"directions": []};
+    let allDirections = {"records": []};
 
     switch(recipeId) {
       case 1:
-        allDirections = {"directions": ["Add ice, lemon juice, creme de cacao, lillet blanc, and gin to shaker and mix thoroughly", 
-        "Strain into chilled nick and nora glass", "Garnish with twist of lemon"]};
+        allDirections = {
+          "records": [
+              {
+                  "id": "recAdXztHCgky2aFm",
+                  "fields": {
+                      "Recipe Direction ID": 3,
+                      "Recipe ID": [
+                          "recwlMsjYThiXto6x"
+                      ],
+                      "Step": 3,
+                      "Direction": "Garnish with twist of lemon"
+                  },
+                  "createdTime": "2020-04-16T17:56:05.000Z"
+              },
+              {
+                  "id": "recUzXp2KGoeMYpoz",
+                  "fields": {
+                      "Recipe Direction ID": 1,
+                      "Recipe ID": [
+                          "recwlMsjYThiXto6x"
+                      ],
+                      "Step": 1,
+                      "Direction": "Add ice, lemon juice, creme de cacao, lillet blanc, and gin to shaker and mix thoroughly"
+                  },
+                  "createdTime": "2020-04-16T17:56:05.000Z"
+              },
+              {
+                  "id": "recyUoBvZsDXTpgge",
+                  "fields": {
+                      "Recipe Direction ID": 2,
+                      "Recipe ID": [
+                          "recwlMsjYThiXto6x"
+                      ],
+                      "Step": 2,
+                      "Direction": "Strain into chilled nick and nora glass"
+                  },
+                  "createdTime": "2020-04-16T17:56:05.000Z"
+              }
+          ]
+      };
         break;
 
       case 2:
+        allDirections = {
+          "records": [
+              {
+                  "id": "recB5z28uK0NAIofz",
+                  "fields": {
+                      "Recipe Direction ID": 6,
+                      "Recipe ID": [
+                          "recZpZn7bWbxauSmP"
+                      ],
+                      "Step": 3,
+                      "Direction": "Garnish with orange peel"
+                  },
+                  "createdTime": "2020-04-16T18:21:56.000Z"
+              },
+              {
+                  "id": "recas8KPBMn6Y02S7",
+                  "fields": {
+                      "Recipe Direction ID": 5,
+                      "Recipe ID": [
+                          "recZpZn7bWbxauSmP"
+                      ],
+                      "Step": 2,
+                      "Direction": "Add ice to double rocks glass and strain in mixture"
+                  },
+                  "createdTime": "2020-04-16T18:21:54.000Z"
+              },
+              {
+                  "id": "recyvHxxvI3rzazH0",
+                  "fields": {
+                      "Recipe Direction ID": 4,
+                      "Recipe ID": [
+                          "recZpZn7bWbxauSmP"
+                      ],
+                      "Step": 1,
+                      "Direction": "Add crushed ice, mezkal, ginger liqueur, and agua fresca to shaker. Mix thoroughly"
+                  },
+                  "createdTime": "2020-04-16T18:21:47.000Z"
+              }
+          ]
+      }
         break;
 
       case 6:
+        allDirections = {
+          "records": [
+              {
+                  "id": "recOrVIyA6kBFyWfs",
+                  "fields": {
+                      "Recipe Direction ID": 19,
+                      "Recipe ID": [
+                          "recuc3BU3F9wzzQGe"
+                      ],
+                      "Step": 1,
+                      "Direction": "Add ice, vodka, schnapps, and lemon juice to shaker. Mix thoroughly"
+                  },
+                  "createdTime": "2020-04-16T19:37:31.000Z"
+              }
+          ]
+      }
         break;
     }
 
--- src/app/Services/recipe-ingredients.service.ts
+++ src/app/Services/recipe-ingredients.service.ts
@@ -32,15 +32,203 @@ export class RecipeIngredientsService extends AirtableService {
   }*/
 
   GetIngredients(recipeId: number): Observable<any> {
-    let allIngredients = {"ingredients": []};
+    let allIngredients = {"records": []};
     
     switch(recipeId) {
       case 1:
-        allIngredients = {"ingredients": ["¾ oz / 22 ½ mL Lemon juice", "½ oz / 15 mL Crème de Cacao", "¾ oz / 22 ½ mL Lillet blanc", 
-        "1 ½ oz / 45 mL London Dry Gin", "Lemon twist"]};
+        allIngredients = {
+          "records": [
+              {
+                  "id": "recBhlYOFCQQFIuoA",
+                  "fields": {
+                      "Recipe Ingredient ID": 5,
+                      "Recipe ID": [
+                          "recwlMsjYThiXto6x"
+                      ],
+                      "Ingredient Name": [
+                          "recb6D3EACfR5l79b"
+                      ],
+                      "Order": 5,
+                      "Ingredient Type": [
+                          "Garnish"
+                      ]
+                  },
+                  "createdTime": "2020-04-16T18:13:57.000Z"
+              },
+              {
+                  "id": "recVwNFepg7xBNRbw",
+                  "fields": {
+                      "Recipe Ingredient ID": 4,
+                      "Recipe ID": [
+                          "recwlMsjYThiXto6x"
+                      ],
+                      "Ounces": 1.5,
+                      "Millilitres": 45,
+                      "Ingredient Name": [
+                          "recY007FNDqkWKhMi"
+                      ],
+                      "Order": 1,
+                      "Ingredient Type": [
+                          "Spirit"
+                      ],
+                      "Ingredient Supertype": [
+                          "Liquor"
+                      ]
+                  },
+                  "createdTime": "2020-04-16T18:09:24.000Z"
+              },
+              {
+                  "id": "receuHNIpNjnyt9FC",
+                  "fields": {
+                      "Recipe Ingredient ID": 2,
+                      "Recipe ID": [
+                          "recwlMsjYThiXto6x"
+                      ],
+                      "Ounces": 0.5,
+                      "Millilitres": 15,
+                      "Ingredient Name": [
+                          "recUNIMdCFZS8R8Yw"
+                      ],
+                      "Order": 3,
+                      "Ingredient Type": [
+                          "Liqueur"
+                      ],
+                      "Ingredient Supertype": [
+                          "Liquor"
+                      ]
+                  },
+                  "createdTime": "2020-04-16T17:55:47.000Z"
+              },
+              {
+                  "id": "reciB4lgFK1EptGZe",
+                  "fields": {
+                      "Recipe Ingredient ID": 1,
+                      "Recipe ID": [
+                          "recwlMsjYThiXto6x"
+                      ],
+                      "Ounces": 0.8,
+                      "Millilitres": 22.5,
+                      "Ingredient Name": [
+                          "recS2s9pI3CUhS0LU"
+                      ],
+                      "Order": 4,
+                      "Ingredient Type": [
+                          "Juice"
+                      ]
+                  },
+                  "createdTime": "2020-04-16T17:55:47.000Z"
+              },
+              {
+                  "id": "recnP2sdTqA4Ei9Qc",
+                  "fields": {
+                      "Recipe Ingredient ID": 3,
+                      "Recipe ID": [
+                          "recwlMsjYThiXto6x"
+                      ],
+                      "Ounces": 0.75,
+                      "Millilitres": 22.5,
+                      "Ingredient Name": [
+                          "rec676s3VXPY2DRYI"
+                      ],
+                      "Order": 2,
+                      "Ingredient Type": [
+                          "Wine"
+                      ],
+                      "Ingredient Supertype": [
+                          "Liquor"
+                      ]
+                  },
+                  "createdTime": "2020-04-16T17:55:47.000Z"
+              }
+          ]
+      };
         break;
 
       case 2:
+        allIngredients = {
+          "records": [
+              {
+                  "id": "recEQaAQjugUDKAmQ",
+                  "fields": {
+                      "Recipe Ingredient ID": 7,
+                      "Recipe ID": [
+                          "recZpZn7bWbxauSmP"
+                      ],
+                      "Ounces": 0.75,
+                      "Millilitres": 22.5,
+                      "Ingredient Name": [
+                          "recH80pEawPq2LNs8"
+                      ],
+                      "Order": 2,
+                      "Ingredient Type": [
+                          "Liqueur"
+                      ],
+                      "Ingredient Supertype": [
+                          "Liquor"
+                      ]
+                  },
+                  "createdTime": "2020-04-16T18:20:13.000Z"
+              },
+              {
+                  "id": "recJS3cs6miFOLbBj",
+                  "fields": {
+                      "Recipe Ingredient ID": 8,
+                      "Recipe ID": [
+                          "recZpZn7bWbxauSmP"
+                      ],
+                      "Ounces": 2.5,
+                      "Millilitres": 75,
+                      "Ingredient Name": [
+                          "rec3BORebOryMPymD"
+                      ],
+                      "Order": 3,
+                      "Ingredient Type": [
+                          "Mixer"
+                      ]
+                  },
+                  "createdTime": "2020-04-16T18:20:50.000Z"
+              },
+              {
+                  "id": "recLieREKEouI4maf",
+                  "fields": {
+                      "Recipe Ingredient ID": 9,
+                      "Recipe ID": [
+                          "recZpZn7bWbxauSmP"
+                      ],
+                      "Ingredient Name": [
+                          "rectG8vbyx6ezUGvF"
+                      ],
+                      "Order": 4,
+                      "Ingredient Type": [
+                          "Garnish"
+                      ]
+                  },
+                  "createdTime": "2020-04-16T18:22:17.000Z"
+              },
+              {
+                  "id": "recY6VYkWsoV58GVm",
+                  "fields": {
+                      "Recipe Ingredient ID": 6,
+                      "Recipe ID": [
+                          "recZpZn7bWbxauSmP"
+                      ],
+                      "Ounces": 1.5,
+                      "Millilitres": 45,
+                      "Ingredient Name": [
+                          "reckYnaVx3sIqb04W"
+                      ],
+                      "Order": 1,
+                      "Ingredient Type": [
+                          "Spirit"
+                      ],
+                      "Ingredient Supertype": [
+                          "Liquor"
+                      ]
+                  },
+                  "createdTime": "2020-04-16T18:18:56.000Z"
+              }
+          ]
+      }
         break;
 
       case 6:
