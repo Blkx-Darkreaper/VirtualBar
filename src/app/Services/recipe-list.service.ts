a0d9f8270461b7805b4b62242888226f02b59a3c
Nicolaj Bunting <steelmagnum@hotmail.com>
2020-07-16 15:09:32 -0700

    Added method to allow for Contains behaviour in searches

--- src/app/Services/recipe-list.service.ts
+++ src/app/Services/recipe-list.service.ts
@@ -29,38 +29,45 @@ export class RecipeListService extends AirtableService {
       let url = this.url as string;
       url += this.requestUrl;
 
+      // console.log("Types(" + allTypes + ")"); //debug
+      // console.log("Occassions(" + allOccassions + ")"); //debug
+      // console.log("Styles(" + allStyles + ")"); //debug
+      // console.log("Families(" + allFamilies + ")"); //debug
+      // console.log("Primary(" + allPrimaryComponents + ")"); //debug
+      // console.log("Secondary(" + allSecondaryComponents + ")"); //debug
+
       let totalFilters = 0;
       let typeFilter: string = "", occassionsFilter: string = "", styleFilter: string = "", familyFilter: string = "", 
       primaryFilter: string = "", secondaryFilter: string = "";
 
       if(allTypes.length > 1 || allTypes[0].toLowerCase() != "all") {
         totalFilters++;
-        typeFilter = this.AppendFieldFilters(typeFilter, "Type", allTypes);
+        typeFilter = this.AppendExactFieldFilters(typeFilter, "Type", allTypes);
       }
 
-      if(allOccassions.length > 1 || allTypes[0].toLowerCase() != "all") {
+      if(allOccassions.length > 1 || allOccassions[0].toLowerCase() != "all") {
         totalFilters++;
-        occassionsFilter = this.AppendFieldFilters(occassionsFilter, "Occassions", allOccassions);
+        occassionsFilter = this.AppendExactFieldFilters(occassionsFilter, "Occassions", allOccassions);
       }
 
       if(allStyles.length > 1 || allStyles[0].toLowerCase() != "all") {
         totalFilters++;
-        styleFilter = this.AppendFieldFilters(styleFilter, "Prep Style", allStyles);
+        styleFilter = this.AppendExactFieldFilters(styleFilter, "Prep Style", allStyles);
       }
 
       if(allFamilies.length > 1 || allFamilies[0].toLowerCase() != "all") {
         totalFilters++;
-        familyFilter = this.AppendFieldFilters(familyFilter, "Family", allFamilies);
+        familyFilter = this.AppendExactFieldFilters(familyFilter, "Family", allFamilies);
       }
 
       if(allPrimaryComponents.length > 1 || allPrimaryComponents[0].toLowerCase() != "all") {
         totalFilters++;
-        primaryFilter = this.AppendFieldFilters(primaryFilter, "Primary Components", allPrimaryComponents);
+        primaryFilter = this.AppendExactFieldFilters(primaryFilter, "Primary Components", allPrimaryComponents);
       }
 
       if(allSecondaryComponents.length > 1 || allSecondaryComponents[0].toLowerCase() != "all") {
         totalFilters++;
-        secondaryFilter = this.AppendFieldFilters(secondaryFilter, "Secondary Components", allSecondaryComponents);
+        secondaryFilter = this.AppendContainsFieldFilters(secondaryFilter, "Secondary Components", allSecondaryComponents);
       }
 
       if(totalFilters > 0) {
@@ -97,7 +104,7 @@ export class RecipeListService extends AirtableService {
         return superFilter;
     }
 
-    private AppendFieldFilters(filter: string, fieldName: string, allFieldValues: string[]): string {
+    private AppendExactFieldFilters(filter: string, fieldName: string, allFieldValues: string[]): string {
       // Add filters
       for(let i in allFieldValues) {
         let fieldValue = allFieldValues[i];
@@ -107,6 +114,26 @@ export class RecipeListService extends AirtableService {
         }
 
         filter += "{" + fieldName + "}='" + fieldValue + "'";
+      }
+
+      if(allFieldValues.length > 1) {
+        filter = "Or(" + filter + ")";
+      }
+
+      return filter;
+    }
+
+    private AppendContainsFieldFilters(filter: string, fieldName: string, allFieldValues: string[]): string {
+      // Add filters
+      for(let i in allFieldValues) {
+        let fieldValue = allFieldValues[i];
+
+        if(filter.length > 0) {
+          filter += ",";
+        }
+
+        // Can't find first element in list unless array is converted to string
+        filter += "Find('" + fieldValue + "',ARRAYJOIN({" + fieldName + "}, ','))";
       }
 
       if(allFieldValues.length > 1) {
