22016e40e5f17b3cf993eb749b284b87d7c5c814
Nicolaj Bunting <steelmagnum@hotmail.com>
2020-09-12 19:17:10 -0700

    Added drink photo pulled from service

--- src/app/Services/recipe-info.service.ts
+++ src/app/Services/recipe-info.service.ts
@@ -1,10 +1,29 @@
 import { of, Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { AirtableService } from './airtable.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RecipeInfoService {
+export class RecipeInfoService extends AirtableService {
+  requestUrl: string = 'Recipe%20Names?';
+  nameField: string = 'fields[]=Name';
+  variantField: string = 'fields[]=Variant';
+  attachmentField: string = 'fields[]=Attachments';
+  recipeFilter: string = 'filterByFormula={Recipe%20ID}=';
 
-  constructor() { }
+  constructor(http: HttpClient) { super(http) }
+
+  GetRecipeImagesFromAirtable(recipeId: number): Observable<any> {
+    let url = this.url as string;
+    url += this.requestUrl
+
+    url += this.nameField;
+    url += '&' + this.variantField;
+    url += '&' + this.attachmentField;
+
+    url += '&' + this.recipeFilter + recipeId;
+    return this.getRequest(url);
+  }
 }
--- src/app/recipe-info/recipe-info.component.ts
+++ src/app/recipe-info/recipe-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { RecipeInfoService } from '../Services/recipe-info.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-recipe-info',
@@ -9,11 +11,45 @@ export class RecipeInfoComponent implements OnInit {
   @Input() id: number;
   @Input() name: string;
   @Input() variant: string;
-  allPhotos: HTMLImageElement[];
+  allImageLinks: URL[];
 
-  constructor(/*private infoService: RecipeInfoService*/) { }
+  constructor(private infoService: RecipeInfoService) { }
 
   ngOnInit(): void {
+    this.allImageLinks = [];
   }
 
+  ngOnChanges(): void {
+    this.updateInfo(this.id);
+  }
+
+  private updateInfo(recipeId: number) {
+    this.allImageLinks = [];
+
+    this.infoService.GetRecipeImagesFromAirtable(recipeId)
+    .pipe(
+      map(response => {
+        let allImageLinks = [];
+        if(!("Attachments" in response.records[0].fields)) {
+          return allImageLinks;
+        }
+
+        allImageLinks = response.records[0].fields["Attachments"].map(
+          attachmentObj => {
+            let url: string = attachmentObj.url;
+            // console.log("Url(" + url + ")");  //debug
+
+            let imageLink: URL = new URL(url);
+            return imageLink;
+          }
+        )
+
+      return allImageLinks;
+    }))
+    .subscribe((data: URL[]) => {
+      // console.log("Links(" + data.toString() + ")"); //debug
+
+      this.allImageLinks = data;
+    });
+  }
 }
