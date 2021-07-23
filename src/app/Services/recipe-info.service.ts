f6b01f6cab154cd097e05408c3a413b17ee8ed5e
Blkx-Darkreaper <steelmagnum@hotmail.com>
2021-07-23 14:24:32 -0700

    Updated recipe info

--- src/app/Services/recipe-info.service.ts
+++ src/app/Services/recipe-info.service.ts
@@ -10,14 +10,114 @@ export class RecipeInfoService extends AirtableService {
   requestUrl: string = 'Recipe%20Names?';
   nameField: string = 'fields[]=Name';
   variantField: string = 'fields[]=Variant';
+  occassionsField: string = 'fields[]=Occassion%20Names';
+  prepStylesField: string = 'fields[]=Prep%20Style%20Names';
+  servedField: string = 'fields[]=Served%20Names';
+  glassField: string = 'fields[]=Glass%20Names';
+  cocktailFamilyField: string = 'fields[]=Family%20Names';
+  relatedToField: string = 'fields[]=Related%20to';
+  ouncesField: string = 'fields[]=Total%20Volume%20(oz)';
+  millilitresField: string = 'fields[]=Total%20Volume%20(mL)';
+  notesField: string = 'fields[]=Notes';
+  // Field: string = 'fields[]=';
   attachmentField: string = 'fields[]=Attachments';
   recipeFilter: string = 'filterByFormula={Recipe%20ID}=';
 
   constructor(http: HttpClient) { super(http) }
 
+  GetPrepStylesFromAirtable(recipeId: number): Observable<any> {
+    let url = this.url as string;
+    url += this.requestUrl;
+
+    url += this.prepStylesField;
+
+    url += '&' + this.recipeFilter + recipeId;
+    return this.getRequest(url);
+  }
+
+  GetGlassFromAirtable(recipeId: number): Observable<any> {
+    let url = this.url as string;
+    url += this.requestUrl;
+
+    url += this.glassField;
+
+    url += '&' + this.recipeFilter + recipeId;
+    return this.getRequest(url);
+  }
+
+  GetServedFromAirtable(recipeId: number): Observable<any> {
+    let url = this.url as string;
+    url += this.requestUrl;
+
+    url += this.servedField;
+
+    url += '&' + this.recipeFilter + recipeId;
+    return this.getRequest(url);
+  }
+
+  GetCocktailFamiliesFromAirtable(recipeId: number): Observable<any> {
+    let url = this.url as string;
+    url += this.requestUrl;
+
+    url += this.cocktailFamilyField;
+
+    url += '&' + this.recipeFilter + recipeId;
+    return this.getRequest(url);
+  }
+
+  GetOccassionsFromAirtable(recipeId: number): Observable<any> {
+    let url = this.url as string;
+    url += this.requestUrl;
+
+    url += this.occassionsField;
+
+    url += '&' + this.recipeFilter + recipeId;
+    return this.getRequest(url);
+  }
+
+  GetRelatedToFromAirtable(recipeId: number): Observable<any> {
+    let url = this.url as string;
+    url += this.requestUrl;
+
+    url += this.relatedToField;
+
+    url += '&' + this.recipeFilter + recipeId;
+    return this.getRequest(url);
+  }
+
+  GetOuncesFromAirtable(recipeId: number): Observable<any> {
+    let url = this.url as string;
+    url += this.requestUrl;
+
+    url += this.ouncesField;
+
+    url += '&' + this.recipeFilter + recipeId;
+    return this.getRequest(url);
+  }
+
+  GetMillilitresFromAirtable(recipeId: number): Observable<any> {
+    let url = this.url as string;
+    url += this.requestUrl;
+
+    url += this.millilitresField;
+
+    url += '&' + this.recipeFilter + recipeId;
+    return this.getRequest(url);
+  }
+
+  GetNotesFromAirtable(recipeId: number): Observable<any> {
+    let url = this.url as string;
+    url += this.requestUrl;
+
+    url += this.notesField;
+
+    url += '&' + this.recipeFilter + recipeId;
+    return this.getRequest(url);
+  }
+
   GetRecipeImagesFromAirtable(recipeId: number): Observable<any> {
     let url = this.url as string;
-    url += this.requestUrl
+    url += this.requestUrl;
 
     url += this.nameField;
     url += '&' + this.variantField;
--- src/app/recipe-info/recipe-info.component.ts
+++ src/app/recipe-info/recipe-info.component.ts
@@ -11,12 +11,30 @@ export class RecipeInfoComponent implements OnInit {
   @Input() id: number;
   @Input() name: string;
   @Input() variant: string;
+  allPrepStyles: string[];
+  allGlasses: string[];
+  allIce: string[];
+  allCocktailFamilies: string[];
+  allOccassions: string[];
   allImageLinks: URL[];
+  relatedTo: string;
+  notes: string;
+  ounces: string;
+  millilitres: string;
 
   constructor(private infoService: RecipeInfoService) { }
 
   ngOnInit(): void {
+    this.allPrepStyles = [];
+    this.allGlasses = [];
+    this.allIce = [];
+    this.allCocktailFamilies = [];
+    this.allOccassions = [];
     this.allImageLinks = [];
+    this.relatedTo = "";
+    this.notes = "";
+    this.ounces = "~";
+    this.millilitres = "~";
   }
 
   ngOnChanges(): void {
@@ -24,32 +42,351 @@ export class RecipeInfoComponent implements OnInit {
   }
 
   private updateInfo(recipeId: number) {
+    this.allPrepStyles = [];
+    this.getAllPrepStyles(recipeId);
+
+    this.allGlasses = [];
+    this.getAllGlasses(recipeId);
+
+    this.allIce = [];
+    this.getAllIce(recipeId);
+
+    this.allCocktailFamilies = [];
+    this.getAllCocktailFamilies(recipeId);
+
+    this.allOccassions = [];
+    this.getAllOccassions(recipeId);
+
     this.allImageLinks = [];
+    this.getAllImages(recipeId);
 
-    this.infoService.GetRecipeImagesFromAirtable(recipeId)
-    .pipe(
-      map(response => {
-        let allImageLinks = [];
-        if(!("Attachments" in response.records[0].fields)) {
-          return allImageLinks;
-        }
+    this.relatedTo = "";
+    this.getRelatedTo(recipeId);
 
-        allImageLinks = response.records[0].fields["Attachments"].map(
-          attachmentObj => {
-            let url: string = attachmentObj.url;
-            // console.log("Url(" + url + ")");  //debug
+    this.notes = "";
+    this.getNotes(recipeId);
 
-            let imageLink: URL = new URL(url);
-            return imageLink;
+    this.ounces = "~";
+    this.millilitres = "~";
+    this.getVolume(recipeId);
+  }
+
+  private getAllPrepStyles(recipeId: number) {
+    this.infoService.GetPrepStylesFromAirtable(recipeId)
+      .pipe(
+        map(response => {
+          let allStyles = [];
+
+          for(let i in response.records[0].fields) {
+            let field = response.records[0].fields[i];
+            // console.log("Response(" + field + ")");  //debug
           }
-        )
 
-      return allImageLinks;
-    }))
-    .subscribe((data: URL[]) => {
-      // console.log("Links(" + data.toString() + ")"); //debug
+          if (!("Prep Style Names" in response.records[0].fields)) {
+            return allStyles;
+          }
+          
+          allStyles = response.records[0].fields["Prep Style Names"];
+          // console.log("Prep Styles(" + allStyles.join(', ') + ")"); //debug
 
-      this.allImageLinks = data;
-    });
+          return allStyles;
+        }))
+      .subscribe((data: string[]) => {
+        let filteredList = data.filter(n => n !== null && n !== undefined); // Remove blanks
+          console.log("Prep Styles(" + filteredList.join(', ') + ")"); //debug
+  
+          let sortedList = filteredList.sort((a, b) => a.localeCompare(b));
+  
+          filteredList = sortedList.filter((n, i) => sortedList.indexOf(n) === i); // Remove duplicates
+
+          this.allPrepStyles = filteredList;
+      });
+  }
+
+  private getAllGlasses(recipeId: number) {
+    this.infoService.GetGlassFromAirtable(recipeId)
+      .pipe(
+        map(response => {
+          let allGlasses = [];
+
+          for(let i in response.records[0].fields) {
+            let field = response.records[0].fields[i];
+            // console.log("Response(" + field + ")");  //debug
+          }
+
+          if (!("Glass Names" in response.records[0].fields)) {
+            return allGlasses;
+          }
+          
+          allGlasses = response.records[0].fields["Glass Names"];
+          // console.log("Glasses(" + allGlasses.join(', ') + ")"); //debug
+
+          return allGlasses;
+        }))
+      .subscribe((data: string[]) => {
+        let filteredList = data.filter(n => n !== null && n !== undefined); // Remove blanks
+          console.log("Glasses(" + filteredList.join(', ') + ")"); //debug
+  
+          let sortedList = filteredList.sort((a, b) => a.localeCompare(b));
+  
+          filteredList = sortedList.filter((n, i) => sortedList.indexOf(n) === i); // Remove duplicates
+
+          this.allGlasses = filteredList;
+      });
+  }
+
+  private getAllIce(recipeId: number) {
+    this.infoService.GetServedFromAirtable(recipeId)
+      .pipe(
+        map(response => {
+          let allIce = [];
+
+          for(let i in response.records[0].fields) {
+            let field = response.records[0].fields[i];
+            // console.log("Response(" + field + ")");  //debug
+          }
+
+          if (!("Served Names" in response.records[0].fields)) {
+            return allIce;
+          }
+          
+          allIce = response.records[0].fields["Served Names"];
+          // console.log("Ice(" + allIce.join(', ') + ")"); //debug
+
+          return allIce;
+        }))
+      .subscribe((data: string[]) => {
+        let filteredList = data.filter(n => n !== null && n !== undefined); // Remove blanks
+          console.log("Ice(" + filteredList.join(', ') + ")"); //debug
+  
+          let sortedList = filteredList.sort((a, b) => a.localeCompare(b));
+  
+          filteredList = sortedList.filter((n, i) => sortedList.indexOf(n) === i); // Remove duplicates
+
+          this.allIce = filteredList;
+      });
+  }
+
+  private getAllCocktailFamilies(recipeId: number) {
+    this.infoService.GetCocktailFamiliesFromAirtable(recipeId)
+      .pipe(
+        map(response => {
+          let allFamilies = [];
+
+          for(let i in response.records[0].fields) {
+            let field = response.records[0].fields[i];
+            // console.log("Response(" + field + ")");  //debug
+          }
+
+          if (!("Family Names" in response.records[0].fields)) {
+            return allFamilies;
+          }
+          
+          allFamilies = response.records[0].fields["Family Names"];
+          // console.log("Families(" + allIce.join(', ') + ")"); //debug
+
+          return allFamilies;
+        }))
+      .subscribe((data: string[]) => {
+        let filteredList = data.filter(n => n !== null && n !== undefined); // Remove blanks
+          console.log("Families(" + filteredList.join(', ') + ")"); //debug
+  
+          let sortedList = filteredList.sort((a, b) => a.localeCompare(b));
+  
+          filteredList = sortedList.filter((n, i) => sortedList.indexOf(n) === i); // Remove duplicates
+
+          this.allCocktailFamilies = filteredList;
+      });
+  }
+
+  private getAllOccassions(recipeId: number) {
+    this.infoService.GetOccassionsFromAirtable(recipeId)
+      .pipe(
+        map(response => {
+          let allOccassions = [];
+
+          for(let i in response.records[0].fields) {
+            let field = response.records[0].fields[i];
+            // console.log("Response(" + field + ")");  //debug
+          }
+
+          if (!("Occassion Names" in response.records[0].fields)) {
+            return allOccassions;
+          }
+          
+          allOccassions = response.records[0].fields["Occassion Names"];
+          // console.log("Occassions(" + allOccassions.join(', ') + ")"); //debug
+
+          return allOccassions;
+        }))
+      .subscribe((data: string[]) => {
+        let filteredList = data.filter(n => n !== null && n !== undefined); // Remove blanks
+          console.log("Occassions(" + filteredList.join(', ') + ")"); //debug
+  
+          let sortedList = filteredList.sort((a, b) => a.localeCompare(b));
+  
+          filteredList = sortedList.filter((n, i) => sortedList.indexOf(n) === i); // Remove duplicates
+
+          if(filteredList.length == 0) {
+            filteredList.push("None");
+          }
+  
+          this.allOccassions = filteredList;
+      });
+
+      // this.infoService.GetOccassionsFromAirtable(recipeId)
+      // .pipe(
+      //   map(response => {
+      //     let allValues = response.records.map(
+      //       obj => {
+      //         return obj.fields["Occassion Names"];
+      //       }
+      //     );
+
+      //     return allValues;
+      //   }))
+      //   .subscribe((data: string[]) => {
+      //     let filteredList = data.filter(n => n !== null && n !== undefined); // Remove blanks
+      //     console.log("Occassions(" + filteredList.join(', ') + ")"); //debug
+  
+      //     let sortedList = filteredList.sort((a, b) => a.localeCompare(b));
+  
+      //     filteredList = sortedList.filter((n, i) => sortedList.indexOf(n) === i); // Remove duplicates
+  
+      //     this.allOccassions = filteredList;
+      //   });
+  }
+
+  private getAllImages(recipeId: number) {
+    this.infoService.GetRecipeImagesFromAirtable(recipeId)
+      .pipe(
+        map(response => {
+          let allImageLinks = [];
+
+          if (!("Attachments" in response.records[0].fields)) {
+            return allImageLinks;
+          }
+
+          allImageLinks = response.records[0].fields["Attachments"].map(
+            attachmentObj => {
+              let url: string = attachmentObj.url;
+              // console.log("Url(" + url + ")");  //debug
+              let imageLink: URL = new URL(url);
+              return imageLink;
+            }
+          );
+
+          return allImageLinks;
+        }))
+      .subscribe((data: URL[]) => {
+        // console.log("Links(" + data.toString() + ")"); //debug
+        this.allImageLinks = data;
+      });
+  }
+
+  private getRelatedTo(recipeId: number) {
+    this.infoService.GetRelatedToFromAirtable(recipeId)
+      .pipe(
+        map(response => {
+          let relatedTo = "";
+
+          for(let i in response.records[0].fields) {
+            let field = response.records[0].fields[i];
+            // console.log("Response(" + field + ")");  //debug
+          }
+
+          if (!("Related to" in response.records[0].fields)) {
+            return relatedTo;
+          }
+          
+          relatedTo = response.records[0].fields["Related to"];
+          // console.log("Related to(" + relatedTo + ")"); //debug
+
+          return relatedTo;
+        }))
+      .subscribe((data: string) => {
+          this.relatedTo = data;
+      });
+  }
+
+  private getNotes(recipeId: number) {
+    this.infoService.GetNotesFromAirtable(recipeId)
+      .pipe(
+        map(response => {
+          let notes = "";
+
+          for(let i in response.records[0].fields) {
+            let field = response.records[0].fields[i];
+            // console.log("Response(" + field + ")");  //debug
+          }
+
+          if (!("Notes" in response.records[0].fields)) {
+            return notes;
+          }
+          
+          notes = response.records[0].fields["Notes"];
+          // console.log("Notes(" + notes + ")"); //debug
+
+          return notes;
+        }))
+      .subscribe((data: string) => {
+          this.notes = data;
+      });
+  }
+
+  private getVolume(recipeId: number) {
+    this.infoService.GetOuncesFromAirtable(recipeId)
+      .pipe(
+        map(response => {
+          let oz = "~";
+
+          for(let i in response.records[0].fields) {
+            let field = response.records[0].fields[i];
+            // console.log("Response(" + field + ")");  //debug
+          }
+
+          if (!("Total Volume (oz)" in response.records[0].fields)) {
+            return oz;
+          }
+          
+          let ozNum = response.records[0].fields["Total Volume (oz)"];
+          // console.log("Oz(" + ozNum + ")"); //debug
+
+          if(isNaN(ozNum) != true) {
+            oz = String(Math.round(ozNum * 100) / 100);
+          }
+
+          return oz;
+        }))
+      .subscribe((data: string) => {
+          this.ounces = data;
+      });
+
+      this.infoService.GetMillilitresFromAirtable(recipeId)
+      .pipe(
+        map(response => {
+          let mils = "~";
+
+          for(let i in response.records[0].fields) {
+            let field = response.records[0].fields[i];
+            // console.log("Response(" + field + ")");  //debug
+          }
+
+          if (!("Total Volume (mL)" in response.records[0].fields)) {
+            return mils;
+          }
+          
+          let milNum = response.records[0].fields["Total Volume (mL)"];
+          // console.log("mL(" + milNum + ")"); //debug
+
+          if(isNaN(milNum) != true) {
+            mils = String(Math.round(milNum * 100) / 100);;
+          }
+
+          return mils;
+        }))
+      .subscribe((data: string) => {
+          this.millilitres = data;
+      });
   }
 }
