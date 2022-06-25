016b3124eac28eef1eaecbd23f83a9385362f41d
Blkx-Darkreaper <steelmagnum@hotmail.com>
2022-06-24 17:08:08 -0700

    Updated inventory service to pull from simplified table by user ID instead of address. Added method to get liquor ingredient names

--- src/app/Services/inventory.service.ts
+++ src/app/Services/inventory.service.ts
@@ -9,27 +9,33 @@ import { HttpClient } from '@angular/common/http';
 export class InventoryService extends AirtableService {
   inventoryUrl: string = 'Inventory?';
   simpleInventoryUrl: string = 'Simple%20Inventory?';
+  usersUrl: string = 'Users?';
 
   addrField: string = 'fields[]=Address';
+  volumeField: string = 'fields[]=Current Volume (mL)';
+
+  userIdField: string = 'fields[]=ID';
   nameField: string = 'fields[]=Name';
   brandField: string = 'fields[]=Brand';
   descField: string = 'fields[]=Description';
-  volumeField: string = 'fields[]=Current Volume (mL)';
   typeField: string = 'fields[]=Type';
   subTypeField: string = 'fields[]=Subtype';
   ingredientTypeField: string = 'fields[]=Ingredient Type Name';
   ingredientNamesField: string = 'fields[]=Ingredient Names';
 
   brandFilter: string = '{Brand}=';
-  idescFilter: string = '{Description}=';
-  addrFilter: string = '{Address}=';
+  descFilter: string = '{Description}=';
+  userFilter: string = '{User ID}=';
   typeFilter: string = '{Type}';
+  addrFilter: string = '{Address}=';
+
+  activeUsersFilter: string = "filterByFormula={Status}='Active'"
 
   filterPrefix: string = 'filterByFormula=And(';
   filterIngredientNamesPrefix = 'Or('
   filterFindPrefix: string = 'FIND(';
   filterFindSuffix: string = '{Ingredient Types})';
-  statusFilter: string = '{Available}=True()'
+  statusFilter: string = '{Available}=True()';
 
   constructor(http: HttpClient) { super(http); }
 
@@ -37,12 +43,19 @@ export class InventoryService extends AirtableService {
     // let url = this.url as string;
     // url += this.inventoryUrl + this.addrField;
     // return this.getRequest(url);
+
+    let url = this.url as string;
+    url += this.usersUrl + this.userIdField;
+
+    url += '&' + this.activeUsersFilter;
+
+    return this.getRequest(url);
     
-    return of(JSON.parse('{"records": [{"id": "rec3yTiBoVOawVAK8","fields": {"Address": "4021 Century Rd"},\
-"createdTime": "2020-04-17T01:03:56.000Z"}]}'));  //testing
+//     return of(JSON.parse('{"records": [{"id": "rec3yTiBoVOawVAK8","fields": {"Address": "4021 Century Rd"},\
+// "createdTime": "2020-04-17T01:03:56.000Z"}]}'));  //testing
   }
 
-  GetLiquorTypesFromAirtable(address: string): Observable<any> {
+  GetLiquorTypesFromAirtable(userId: number): Observable<any> {
     let url = this.url as string;
     url += this.simpleInventoryUrl;
 
@@ -52,12 +65,12 @@ export class InventoryService extends AirtableService {
     url += '&' + this.subTypeField;
     url += '&' + this.ingredientTypeField;
 
-    url += '&' + this.filterPrefix + this.addrFilter + "'" + address + "')";
+    url += '&' + this.filterPrefix + this.userFilter + "'" + userId + "')";
 
     return this.getRequest(url);
   }
 
-  GetSpiritLiquorTypesFromAirtable(address: string): Observable<any> {
+  GetSpiritLiquorTypesFromAirtable(userId: number): Observable<any> {
     let url = this.url as string;
     url += this.simpleInventoryUrl;
 
@@ -69,14 +82,16 @@ export class InventoryService extends AirtableService {
     url += '&' + this.ingredientTypeField;
     url += '&' + this.ingredientNamesField;
 
-    url += '&' + this.filterPrefix + this.addrFilter + "'" + address + "'";
+    url += '&' + this.filterPrefix + this.userFilter + "'" + userId + "'";
     url += ',' + this.typeFilter + "='Spirit'";
     url += ',' + this.statusFilter + ')';
 
+    // console.log('GetSpiritLiquorTypes(' + url + ')');  //debug
+
     return this.getRequest(url);
   }
 
-  GetNonSpiritLiquorTypesFromAirtable(address: string): Observable<any> {
+  GetNonSpiritLiquorTypesFromAirtable(userId: number): Observable<any> {
     let url = this.url as string;
     url += this.simpleInventoryUrl;
 
@@ -88,9 +103,31 @@ export class InventoryService extends AirtableService {
     url += '&' + this.ingredientTypeField;
     url += '&' + this.ingredientNamesField;
 
-    url += '&' + this.filterPrefix + this.addrFilter + "'" + address + "'";
+    url += '&' + this.filterPrefix + this.userFilter + "'" + userId + "'";
     url += ',' + this.typeFilter + "!='Spirit'";
     url += ',' + this.statusFilter + ')';
+
+    // console.log('GetNonSpiritLiquorTypes(' + url + ')');  //debug
+
+    return this.getRequest(url);
+  }
+
+  GetLiquorIngredientNamesFromAirtable(userId: number): Observable<any> {
+    let url = this.url as string;
+    url += this.simpleInventoryUrl;
+
+    url += this.nameField;
+    url += '&' + this.brandField;
+    url += '&' + this.descField;
+    url += '&' + this.typeField;
+    url += '&' + this.subTypeField;
+    url += '&' + this.ingredientTypeField;
+    url += '&' + this.ingredientNamesField;
+
+    url += '&' + this.filterPrefix + this.userFilter + "'" + userId + "'";
+    url += ',' + this.statusFilter + ')';
+
+    // console.log('GetLiquorIngredientNames(' + url + ')');  //debug
 
     return this.getRequest(url);
   }
@@ -100,8 +137,8 @@ export class InventoryService extends AirtableService {
     url += this.inventoryUrl;
 
     url += this.brandFilter + brand;
-    url += this.idescFilter + desc;
-    url += this.addrFilter + address;
+    url += this.descFilter + desc;
+    url += this.userFilter + address;
 
     url += this.volumeField;
 
@@ -113,10 +150,10 @@ export class InventoryService extends AirtableService {
 
     url += this.brandField;
     url += '&' + this.descField;
-    url += '&' + this.addrField;
+    url += '&' + this.userIdField;
     url += '&' + this.volumeField;
 
-    url += '&' + this.filterPrefix + this.addrFilter + "'" + address;
+    url += '&' + this.filterPrefix + this.userFilter + "'" + address;
 
     let filterUrl = '';
     for(let i in allIngredientNames) {
@@ -148,7 +185,7 @@ export class InventoryService extends AirtableService {
     // url += '&' + this.addrField;
     url += '&' + this.volumeField;
 
-    url += '&' + this.filterPrefix + this.addrFilter + "'" + address;
+    url += '&' + this.filterPrefix + this.userFilter + "'" + address;
     url += "'," + this.filterFindPrefix + "'" + ingredientName + "'" + this.filterFindSuffix
     url += ')';
 
