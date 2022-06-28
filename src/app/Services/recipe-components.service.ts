6c853acffdc74621defb73e7e11d398da1f6602e
Blkx-Darkreaper <steelmagnum@hotmail.com>
2022-06-28 12:39:56 -0700

    Tidied up code

--- src/app/Services/recipe-components.service.ts
+++ src/app/Services/recipe-components.service.ts
@@ -1,5 +1,5 @@
 import { AirtableService } from './airtable.service';
-import { of, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -8,31 +8,40 @@ import { HttpClient } from '@angular/common/http';
 })
 export class RecipeComponentsService extends AirtableService {
   ingTypeRequestUrl: string = 'Ingredient%20Types';
-  fieldsFilter: string = '?fields[]=Name';
-  primaryFilter: string = '&filterByFormula=And({Type}="Spirit",Len({Subtype})>0,Len({Category})=0)';
+  fieldPrefix: string = '?fields[]=';
+  nameField: string = 'Name';
+  filterByFormula: string = '&filterByFormula='
+  primaryFilter: string = 'And({Type}="Spirit",Len({Subtype})>0,Len({Category})=0)';
   // secondaryFilter: string = '?filterByFormula=And({Supertype}="Liquor",{Qualifier}=Blank())';
   // secondaryFilter: string = '?filterByFormula={Supertype}="Liquor"';
-  secondaryFilter: string = '&filterByFormula=And({Supertype}="Liquor",{Type}!="Spirit",Len({Subtype})>0,Len({Category})=0)';
+  secondaryFilter: string = 'And({Supertype}="Liquor",{Type}!="Spirit",Len({Subtype})>0,Len({Category})=0)';
+  
   ingRequestUrl: string = 'Ingredients';
-  tertiaryFilter: string = '&filterByFormula={Supertype}!="Liquor"';
+  tertiaryFilter: string = '{Supertype}!="Liquor"';
 
   constructor(http: HttpClient) { super(http) }
 
   GetPrimariesFromAirtable(): Observable<any> {
     let url = this.url as string;
-    url += this.ingTypeRequestUrl + this.fieldsFilter + this.primaryFilter;
+    url += this.ingTypeRequestUrl + this.fieldPrefix + this.nameField + this.filterByFormula + this.primaryFilter;
     return this.getRequest(url);
   }
 
   GetSecondariesFromAirtable(): Observable<any> {
     let url = this.url as string;
-    url += this.ingTypeRequestUrl + this.fieldsFilter + this.secondaryFilter;
+    url += this.ingTypeRequestUrl + this.fieldPrefix + this.nameField + this.filterByFormula + this.secondaryFilter;
     return this.getRequest(url);
   }
 
-  GetTertiariesFromAirtable(): Observable<any> {
+  /* GetTertiariesFromAirtable(): Observable<any> {
     let url = this.url as string;
     url += this.ingRequestUrl + this.fieldsFilter + this.tertiaryFilter;
+    return this.getRequest(url);
+  } */
+
+  GetIngredientTypesFromAirtable(fieldName: string): Observable<any> {
+    let url = this.url as string;
+    url += this.ingRequestUrl + this.fieldPrefix + fieldName;
     return this.getRequest(url);
   }
 }
